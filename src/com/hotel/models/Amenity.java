710e98bb30d2911ddbd898929fca5ccd2cd5d4ea
seif <seifahmed01376@gmail.com>
2026-04-05 19:09:52 +0200

    Amenity class & RoomType class

---
+++ src/com/hotel/models/Amenity.java
@@ -0,0 +1,31 @@
+package com.hotel.models;
+
+public class Amenity {
+    private int id;
+    private String amenity;
+
+    Amenity(int id,String amenity){
+        this.id=id;
+        this.amenity=amenity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAmenity() {
+        return amenity;
+    }
+
+    public void setAmenity(String amenity) {
+        this.amenity = amenity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Override
+    public String toString(){
+        return "Amenity name: "+ amenity +"-Amenity id: "+id;
+    }
+}
